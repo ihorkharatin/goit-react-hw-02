0e13a8c3a8f3baefe9b1bf659ef4c101ef28a68e
Ihor Kharatin <ikharatin44@gmail.com>
2024-08-10 21:07:53 +0200

    description

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const REVIEWS_KEY = "reviews_key";
+  const [reviews, setReviews] = useState(() => {
+    const reviewsState = window.localStorage.getItem(REVIEWS_KEY);
+
+    if (reviewsState !== null) {
+      return JSON.parse(reviewsState);
+    }
+
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
+  const totalFeedback = Object.values(reviews).reduce(
+    (acc, value) => acc + value,
+    0
+  );
+  const positiveFeedback = totalFeedback
+    ? Math.round((reviews.good / totalFeedback) * 100)
+    : 0;
+
+  const updateFeedback = (feedbackType) => {
+    setReviews((prev) => ({ ...prev, [feedbackType]: prev[feedbackType] + 1 }));
+  };
+
+  const resetReviews = () => {
+    setReviews({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem(REVIEWS_KEY, JSON.stringify(reviews));
+  }, [reviews]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Description />
+      <Options
+        reviews={Object.keys(reviews)}
+        handleClick={updateFeedback}
+        totalFeedback={totalFeedback}
+        handleReset={resetReviews}
+      />
+      {totalFeedback === 0 ? (
+        <Notification />
+      ) : (
+        <Feedback
+          reviews={Object.entries(reviews)}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
